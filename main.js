0241bdb0ff336edd28e5449b5fec469c61d3c635
John Dawkins <jrdawkins88@gmail.com>
2016-06-15 16:33:23 -0400

    Uncommented out Exercises 1-4

--- main.js
+++ main.js
@@ -81,35 +81,35 @@ var alyssa = {
 var people = [amanda, brian, terrence, alyssa];
 
 // // Exercise 1 - Add a child with a name and age to each parent. 
-// amanda.children[amanda.children.length] = { name: 'Josh', age: 18, gender: 'F' };
-// brian.children[brian.children.length] = { name: 'Timmy', age: 4, gender: 'M' };
-// terrence.children[terrence.children.length] = { name: 'Lil T', age: 5, gender: 'M' };
-// alyssa.children[alyssa.children.length] = { name: 'Zoey', age: 4, gender: 'F' };
+amanda.children[amanda.children.length] = { name: 'Josh', age: 18, gender: 'F' };
+brian.children[brian.children.length] = { name: 'Timmy', age: 4, gender: 'M' };
+terrence.children[terrence.children.length] = { name: 'Lil T', age: 5, gender: 'M' };
+alyssa.children[alyssa.children.length] = { name: 'Zoey', age: 4, gender: 'F' };
 
 // // Exercise 2 increasing the age of each 'M' parent by five years
-// for (var i = 0; i < people.length; i++) {
-// 	 if (people[i].gender ==='M') {
-// 	 	people[i]['age'] += 5; 
-// 	 } 
-// 	console.log(people[i].firstName, people[i].age);
-// }
+for (var i = 0; i < people.length; i++) {
+	 if (people[i].gender ==='M') {
+	 	people[i]['age'] += 5; 
+	 } 
+	console.log(people[i].firstName, people[i].age);
+}
 // // // Exercise 3 
-// var ageSum = 0; 
-// var averageAge; 
+var ageSum = 0; 
+var averageAge; 
 
-// for (var i = 0; i < people.length; i++) {
-// 	ageSum += people[i].age;
-// }
-// averageAge = ageSum / people.length;
-// console.log('average age :', averageAge);
+for (var i = 0; i < people.length; i++) {
+	ageSum += people[i].age;
+}
+averageAge = ageSum / people.length;
+console.log('average age :', averageAge);
 
 // // // Exercise 4
-// for (var i = 0; i < people.length; i++) {
-// 	for (var j = 0; j < people[i].children.length; j++) {
-// 		console.log(people[i].children[j].name + '(' + people[i].children[j].age + ',' + people[i].children[j].gender + ')');
-// 	}
-// 	// console.log(people[i].children);
-// }
+for (var i = 0; i < people.length; i++) {
+	for (var j = 0; j < people[i].children.length; j++) {
+		console.log(people[i].children[j].name + '(' + people[i].children[j].age + ',' + people[i].children[j].gender + ')');
+	}
+	console.log(people[i].children);
+}
 // Exercise 5
 
 var maleSum = 0;
